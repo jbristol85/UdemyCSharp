586a3b22cdea90eca76a108aa3063cd1a74779a7
jbristol85 <josh.bristol2@gmail.com>
2018-05-20 21:23:42 -0400

    Initial commit

---
+++ TypeConversion/TypeConversion/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            //try
+            //{
+            //    var number = "1234";
+            //    byte b = Convert.ToByte(number);
+            //    Console.WriteLine(b);
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine("The number could not be converted to a byte");
+             
+            //}
+
+            try
+            {
+                string str = "true";
+                bool b = Convert.ToBoolean(str);
+                Console.WriteLine(b);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The number could not be converted to a bool");
+
+            }
+        }
+    }
+}
