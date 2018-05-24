24a48d55992c4220447b65d9664ebccef636964c
jbristol85 <josh.bristol2@gmail.com>
2018-05-24 18:28:39 -0400

    section5Factorial complete

---
+++ section5factorial/section5factorial/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace section5factorial
+{
+    class Program
+    {
+        private static int _total = 1;
+        public static int Factorial(int number)
+        {
+            for (var i = 1; i <= number; i++)
+            {
+                _total *= i;
+            }
+
+            return _total;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number to Factorialize: ");
+            var input = Console.ReadLine();
+
+            var number = Convert.ToInt32(input);
+            Console.WriteLine(Factorial(number));
+            
+        }
+    }
+}
---
+++ section5promptnumbers/section5promptnumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace section5promptnumbers
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            var tally = 0;
+            while (true)
+            {
+                Console.WriteLine("Enter number to tally or \"ok\" to exit: ");
+                var input = Console.ReadLine();
+
+                if (input != "ok")
+                {
+                    int number = Convert.ToInt32(input);
+                    tally += number;
+                    Console.WriteLine("The total is {0}", tally);
+                    continue;
+                }
+                Console.WriteLine("@Echo: " + "Exiting");
+                break;
+
+            }
+        }
+    }
+}
