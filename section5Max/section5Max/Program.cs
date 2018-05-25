8650d4b0e898380d275e8bb226c503f8dbce2442
jbristol85 <josh.bristol2@gmail.com>
2018-05-25 00:17:46 -0400

    finished section 5

---
+++ section5Max/section5Max/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace section5Max
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a series of numbers seperated by a \",\" : ");
+            var input = Console.ReadLine();
+            var nums = input.Split(',');
+
+            var maximum = Convert.ToInt32(nums[0]);
+
+            foreach (var num in nums)
+            {
+                var number = Convert.ToInt32(num);
+                if (number > maximum)
+                {
+                    maximum = number;
+                }
+            }
+
+            Console.WriteLine("The largest number is {0}.", maximum);
+        }
+    }
+}
---
+++ section5Random/section5Random/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace section5Random
+{
+    class Program
+    {
+        private static int numGuesses = 0;
+        static void Main(string[] args)
+        {
+            
+            var rando = new Random();
+            var numToGuess = rando.Next(1, 10);
+            Console.WriteLine(numToGuess);
+
+
+            while (numGuesses < 4)
+            {
+                Console.Write("What number between 1 and 10 am I thinking of: ");
+                var input = Console.ReadLine();
+                var guess = Convert.ToInt32(input);
+                numGuesses++;
+
+                if (numToGuess == guess)
+                {
+                    Console.WriteLine("You Won");
+                    break;
+                }
+                else if (numToGuess != guess && numGuesses < 4)
+                {
+                    Console.WriteLine("Try again");
+                    continue;
+                }
+                else if(numGuesses == 4)
+                {
+                    Console.WriteLine("You Lost. The number was {0}.", numToGuess);
+                }
+                    
+            }
+            
+        }
+    }
+}
