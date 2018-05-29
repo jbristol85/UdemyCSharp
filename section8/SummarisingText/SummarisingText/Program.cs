d8650cdec81273fac5d5a271ffba67d2457ad759
jbristol85 <josh.bristol2@gmail.com>
2018-05-29 01:16:34 -0400

    Adjusted file structure, section 8 summarising text complete

---
+++ section8/SummarisingText/SummarisingText/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummarisingText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence =
+                "The punishment assigned to a defendant found guilty by a court, or fixed by law for a particular offense.";
+            var summary = StringUtility.SummerizeText(sentence, 25);
+            Console.WriteLine(summary);
+        }
+
+    }
+}
